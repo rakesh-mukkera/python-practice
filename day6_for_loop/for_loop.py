9e950f2a88df3a9167734ce6e969384ec4994b09
Mukkera Rakesh <rakeshmukkera226@gmail.com>
2026-07-31 21:40:01 +0530

    Add files via upload

---
+++ day6_for_loop/for_loop.py
@@ -0,0 +1,26 @@
+# Print numbers from 1 to 10 using a for loop.
+for i in range(1,10):
+    print(i)
+
+
+
+    # Write a program to print numbers from 10 to 1 using a for loop
+for i in range(10,0,-1):
+    print(i)
+
+    # Print all even numbers from 1 to 50 using a for loop
+for i in range(1,50):
+    if i%2== 0:
+        print(i)
+
+
+        # Print all odd numbers from 1 to 50 using a for loop.
+for i in range(1,51):
+    if i %2 != 0:
+        print(i)
+
+
+        # Write a program to print the multiplication table of a given number using a for loop.
+num =  5
+for i in range(1,11):
+    print(num,"*",i,"=",num*i)
